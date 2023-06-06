b4b7cfd86c844253454eed64ac44b0a96f1b5e70
Hassene-Belhaj <hassene.belhaj@gmail.com>
2023-06-06 22:05:25 +0200

    add a little animation opacity with framer-motion

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,26 @@ import React from 'react'
 import { GlobalStyle } from './Components/Navbar/Navbar.Style'
 import Hero from './Components/Hero/Hero'
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
-import Section1 from './Components/DynamicSection/DynamicSection'
 import PizzaOfTheDaySection from './Components/PizzaOfTheDaySection/PizzaOfTheDaySection'
 import { Data, DataTwo } from './Components/DynamicSection/Data'
 import Footer from './Components/Footer/Footer'
+import DynamicSection from './Components/DynamicSection/DynamicSection'
 
 
 
 const App = () => {
+
+
+
+  
   return (
 
     <Router>
         <GlobalStyle/>
         <Hero />
-        <Section1   heading={'Choose Your Favorite'} Data={Data} />
+        <DynamicSection   heading={'Choose Your Favorite'} Data={Data} />
         <PizzaOfTheDaySection/>
-        <Section1 heading={'Sweet Treats For You'} Data={DataTwo} />
+        <DynamicSection heading={'Sweet Treats For You'} Data={DataTwo} />
         <Footer />
     </Router>
   )
--- src/Components/DynamicSection/DynamicSection.jsx
+++ src/Components/DynamicSection/DynamicSection.jsx
@@ -1,16 +1,69 @@
 import React from 'react'
 import { SectionContainer, SectionDiv, SectionDivCard, SectionH2, SectionH3, SectionImage, SectionP, SectionP2 } from './DynamicSection.Style'
 import { Button } from '../Hero/Hero.Style'
+import { motion ,useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+import { useEffect } from 'react'
+
+
 
 const DynamicSection = ({heading,Data}) => {
 
+ const {ref,inView} = useInView({
+  threshold : 0.3, 
+ }
+ )
+
+ const animation = useAnimation()
+
+
+ useEffect(()=>{
+if(inView) {
+  animation.start({
+    opacity :  0.9,
+    transition : {
+      delay : 0.2 ,
+      duration : 1.5 ,
+    }
+  })
+  
+} else {
+  animation.start({
+    opacity : 0.3 ,
+  })
+}
+
+ },[inView])
+
+
+
+  const VariantItem = {
+    hidden : {
+        // x : "-100vw"
+        opacity : 0.1 ,
+    },
+    visible :{
+        // x: 0 ,
+        transition : {
+            duration :  5} ,
+        opacity : 1 ,
+    }
+}
+
   return (
    <>
-   <SectionContainer>
+   <SectionContainer ref={ref}>
+    <motion.div 
+    initial="hidden"
+    // animate="visible"
+    // variants={VariantItem}
+    animate ={animation}
+    > 
+
     <SectionH2>{heading}</SectionH2>
    <SectionDiv>
      {Data.map((item,index)=>{
-      return (
+       return (
          <SectionDivCard key={index}>
          <SectionImage src={item.image} alt={item.alt} />
          <SectionH3>{item.title}</SectionH3>
@@ -19,8 +72,9 @@ const DynamicSection = ({heading,Data}) => {
          <Button>Add To Cart</Button>
          </SectionDivCard>
       )
-     })}
+    })}
    </SectionDiv>
+    </motion.div>
    </SectionContainer> 
    </>
 
